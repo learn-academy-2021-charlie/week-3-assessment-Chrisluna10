3ec79ba6462a2196f6792edccecc7934f0c09cd9
Chrisluna10 <chrisluna4224@gmail.com>
2021-07-27 08:48:42 -0700

    completed test challenge

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding practical questions with Jest
 
+const { TestWatcher } = require("jest")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -18,36 +20,84 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("fibo", () => {
+    // a test method that describes what the function will do
+    test("returns array with accumulating sum", () => {
+    // what the expected output will be
+            expect(fibo(6).toEqual([1, 1, 2, 3, 5, 8]))
+            expect(fibo(10).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]))
+            
+    })
+})
+
 // Example input: 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
 // Example input: 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-
-
 // b) Create the function that makes the test pass.
 
-
+// Create a function
+// Parameter - array
+// Iteration - for loop
+const fibo = (number) => {
+    let answerArr = (1,1)
+    for(let i=2; i<number; i++){
+        answerArr.push(answerArr(i-2) + answerArr(i-1))
+    }
+    return answerArr
+}
+//test did not pass and says there is an error in the expect method. Tried running console.log and answerArr was undefined. 
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
+// create a describe method that lists the name of the function
+describe("odd", () => {
+    // a test method that describes what the function will do
+    test("returns array with odd numbers least to greatest", () => {
+    // what the expected output will be
+            expect(odd(fullArr1).toEqual([-9, 7, 9, 199]))
+            expect(odd(fullArr2).toEqual([-823, 7, 23]))
+    })
+})
 
 var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
+// create a function that takes in an array and returns new array.
 
 var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
 
 // b) Create the function that makes the test pass.
-
+const onlyOdd = (array) => {
+    let filteredArr = array.filter(value => {
+      typeof value === "number" && value % 2 !== 0
+       })
+       return onlyOdd.sort((a, b) => a - b)
+  }
+  console.log(onlyOdd.sort((a, b) => a - b))
 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
+// Create a function
+// Parameter - array
+// Iteration - map or for loop
+
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("sum", () => {
+    // a test method that describes what the function will do
+    test("returns array with accumulating sum", () => {
+    // what the expected output will be
+            expect(sum(numbersToAdd1).toEqual([2, 6, 51, 60]))
+            expect(sum(numbersToAdd2).toEqual([0, 7, -1, 11]))
+            expect(sum(numbersToAdd3).toEqual([]))
+    })
+})
 
 var numbersToAdd1 = [2, 4, 45, 9]
 // Excpected output: [2, 6, 51, 60]
@@ -61,3 +111,16 @@ var numbersToAdd3 = []
 
 
 // b) Create the function that makes the test pass.
+// Create a function
+// Parameter - array
+// Iteration - for loop
+const sum = (array) => {
+    if(array.length > 0){
+        let arraySum = [array[0]]
+        for(let i=1; i<array.length; i++){
+        arraySum.push(array[i] + arraySum(i - 1))
+    }
+    return arraySum
+}
+return []
+}
--- interview-questions.md
+++ interview-questions.md
@@ -9,41 +9,41 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: `this` is a term used to refer to an object or a class.
 
-  Researched answer:
+  Researched answer: THIS keyword is the reference variable. It refers to the current object. It can be used to invoke the current class constructor.
 
 
 
 2. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: yarn is a database library that allows for a developer to run tests. JS files ar modified in a React application.
 
-  Researched answer:
+  Researched answer: Yarn is a package manager that doubles down as project manager.
 
 
 
 3. What are props in React?
 
-  Your answer:
+  Your answer: props are global variables that hold information.
 
-  Researched answer:
-
+  Researched answer: Props (properties) is a keyword in React for passing information from one component to another
+ 
 
 
 4. What is JSX?
 
-  Your answer:
+  Your answer: JSX is part of React that is basically a combination of JS,HTML, and CSS.
 
-  Researched answer:
+  Researched answer: JSX is an XML/HTML-like syntax used by React that extends ECMAScript so that XML/HTML-like text can co-exist with JavaScript/React code
 
 
 
 5. What is a DOM event?
 
-  Your answer:
+  Your answer: a DOM is a document object model
 
-  Researched answer:
+  Researched answer: stands for 'Document Object Model'. In simple terms, it is a structured representation of the HTML elements that are present in a webpage or web-app
 
 
 
@@ -51,17 +51,17 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: Anonymous Function is a function that does not have any name associated with it.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: Conditional rendering is a term to describe the ability to render different user interface (UI) markup if a condition is true or false.
 
-2. Ruby:
+2. Ruby: Ruby is an interpreted, high-level, general-purpose programming language.
 
-3. Object oriented programming:
+3. Object oriented programming: Object Oriented programming (OOP) is a programming paradigm that relies on the concept of classes and objects.
 
-4. Ruby hash:
+4. Ruby hash: Hash is a data structure that maintains a set of objects which are termed as the keys and each key associates a value with it
 
-5. Ruby blocks:
+5. Ruby blocks: Ruby blocks are anonymous functions that can be passed into methods
